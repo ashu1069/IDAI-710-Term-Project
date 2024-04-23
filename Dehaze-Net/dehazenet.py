c9eebf7d34b653793f6755698b6d0f0065cd57a6
Ashutosh Kumar <ak1825@rit.edu>
2024-04-23 16:34:05 -0400

    Dehaze-Net

---
+++ Dehaze-Net/dehazenet.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+class DehazeNet(nn.Module):
+    def __init__(self, input=16, groups=4):
+        super(DehazeNet, self).__init__()
+        self.input = input
+        self.groups = groups
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.input, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=4, out_channels=self.input, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=4, out_channels=self.input, kernel_size=5, padding=2)
+        self.conv4 = nn.Conv2d(in_channels=4, out_channels=self.input, kernel_size=7, padding=3)
+        self.maxpool = nn.MaxPool2d(kernel_size=7, stride=1)
+        self.conv5 = nn.Conv2d(in_channels=48, out_channels=1, kernel_size=6)
+
+        for name, m in self.named_modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, mean=0, std=0.001)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def Maxout(self, x, groups):
+        x = x.reshape(x.shape[0], groups, x.shape[1]//groups, x.shape[2], x.shape[3])
+        x, _ = torch.max(x, dim=2, keepdim=True)
+        out = x.reshape(x.shape[0], -1, x.shape[3], x.shape[4])
+        return out
+
+    def BRelu(self, x):
+        zeros = torch.zeros_like(x)
+        ones = torch.ones_like(x)
+        x = torch.max(x, zeros)
+        x = torch.min(x, ones)
+        return x
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.Maxout(out, self.groups)
+        out1 = self.conv2(out)
+        out2 = self.conv3(out)
+        out3 = self.conv4(out)
+        y = torch.cat((out1, out2, out3), dim=1)
+        y = self.maxpool(y)
+        y = self.conv5(y)
+        y = self.BRelu(y)
+        y = y.reshape(y.shape[0], -1)
+        return y
